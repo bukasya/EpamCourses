730924c5fcbd72fe47c4756009cec8d1242ce38a
bukasya <bukasya@live.ru>
2017-02-26 22:04:18 +0300

    remove unnecessary methods

--- unit02/task03/src/main/java/task03/Stationery.java
+++ unit02/task03/src/main/java/task03/Stationery.java
@@ -8,21 +8,8 @@ public abstract class Stationery {
         return this.price;
     }
 
-    public void setPrice(int price){
-        this.price = price;
-    }
-
-    public void setProducer(String producer){
-        this.producer = producer;
-    }
-
     public Stationery(int price, String producer){
         this.price = price;
         this.producer = producer;
-    }
-
-    public Stationery(){
-        this.price = 0;
-        this.producer = "unknown";
     }
 }
