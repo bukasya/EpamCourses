d38aff41df01ef6354093084311650586f38228d
bukasya <bukasya@live.ru>
2017-03-08 20:06:07 +0300

    full task03

--- unit03/task03/src/main/java/task03/ImageParser.java
+++ unit03/task03/src/main/java/task03/ImageParser.java
@@ -1,6 +1,9 @@
 package task03;
 
 import java.io.*;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,24 +12,24 @@ public class ImageParser {
     private Pattern imageNumberPattern = Pattern.compile("\\d");
     private int lastImageNumber;
     private File initialFile = new File("src\\main\\Java.SE.03.Information handling_task_attachment.html");
+    private File receivedFile = new File("src\\main\\received.html");
 
-    public void parseFile(){
+    public void parseFile() {
         String currentLine;
         try {
             FileInputStream fileInputStream = new FileInputStream(initialFile);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "windows-1251");
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            while ((currentLine = bufferedReader.readLine()) != null){
+            while ((currentLine = bufferedReader.readLine()) != null) {
                 Matcher matcher = imageReferencePattern.matcher(currentLine);
-                while(matcher.find()){
+                while (matcher.find()) {
                     //parse image number from imageNumberPattern
                     Matcher intMatcher = imageNumberPattern.matcher(matcher.group());
-                    if(intMatcher.find()){
+                    if (intMatcher.find()) {
                         int parsedValue = Integer.parseInt(intMatcher.group());
-                        if(parsedValue > lastImageNumber){
+                        if (parsedValue > lastImageNumber) {
                             lastImageNumber = parsedValue;
-                        }
-                        else{
+                        } else {
                             //if any next image number doesn't exceed previous
                             System.out.println("The author refers to the figures inconsistently.");
                             return;
@@ -37,6 +40,52 @@ public class ImageParser {
             System.out.println("The author refers to the figures consistently.");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> splitToSentences() {
+        ArrayList<String> sentences = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(initialFile);
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "windows-1251");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            BreakIterator breakIterator = BreakIterator.getSentenceInstance(Locale.getDefault());
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                breakIterator.setText(currentLine);
+                int start = breakIterator.first();
+                for (int end = breakIterator.next(); end != BreakIterator.DONE; start = end, end = breakIterator.next()) {
+                    sentences.add(currentLine.substring(start, end));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentences;
+    }
+
+    public void markSentencesWithImages(ArrayList<String> sentences){
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(receivedFile);
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "windows-1251");
+            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+            for (String s: sentences) {
+                Matcher matcher = imageReferencePattern.matcher(s);
+                if(matcher.find()){
+                    bufferedWriter.write("<b>" + s + "</b>\n");
+                }
+                else{
+                    bufferedWriter.write(s + "\n");
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- unit03/task03/src/test/java/task03/ImageParserTest.java
+++ unit03/task03/src/test/java/task03/ImageParserTest.java
@@ -6,6 +6,18 @@ import static org.junit.Assert.*;
 
 public class ImageParserTest {
     @Test
+    public void testMarkSentencesWithImages() throws Exception {
+        ImageParser imageParser = new ImageParser();
+        imageParser.markSentencesWithImages(imageParser.splitToSentences());
+    }
+
+    @Test
+    public void testSplitToSentences() throws Exception {
+        ImageParser imageParser = new ImageParser();
+        imageParser.splitToSentences();
+    }
+
+    @Test
     public void testParseFile() throws Exception {
         ImageParser imageParser = new ImageParser();
         imageParser.parseFile();
