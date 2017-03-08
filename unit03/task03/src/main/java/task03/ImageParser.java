b5dd58c0b5b1ee6f2a78ff7fed6037ae80c81b91
bukasya <bukasya@live.ru>
2017-03-08 18:48:28 +0300

    check if author refers to the figures consistently

--- unit03/task03/src/main/java/task03/ImageParser.java
+++ unit03/task03/src/main/java/task03/ImageParser.java
@@ -7,13 +7,11 @@ import java.util.regex.Pattern;
 public class ImageParser {
     private Pattern pattern = Pattern.compile("\\([Рр]ис\\.\\s\\d+\\)");
     private Pattern intPattern = Pattern.compile("\\d");
-    private String[] imageNumbers = new String[200];
-    private int[] intNumbers = new int[200];
+    private int lastNumber;
     private File file = new File("D:\\Java\\EpamCourses\\unit03\\task03\\src\\main\\Java.SE.03.Information handling_task_attachment.html");
 
     public void parseFile(){
         String currentLine;
-        int i = 0;
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(fileInputStream, "windows-1251");
@@ -21,17 +19,20 @@ public class ImageParser {
             while ((currentLine = bufferedReader.readLine()) != null){
                 Matcher matcher = pattern.matcher(currentLine);
                 while(matcher.find()){
-                    imageNumbers[i] = matcher.group();
-                    Matcher intMatcher = intPattern.matcher(imageNumbers[i]);
-                    while(intMatcher.find()){
-                        intNumbers[i] = Integer.parseInt(intMatcher.group());
+                    Matcher intMatcher = intPattern.matcher(matcher.group());
+                    if(intMatcher.find()){
+                        int parsedValue = Integer.parseInt(intMatcher.group());
+                        if(parsedValue > lastNumber){
+                            lastNumber = parsedValue;
+                        }
+                        else{
+                            System.out.println("The author refers to the figures inconsistently.");
+                            return;
+                        }
                     }
-                    i++;
                 }
             }
-            for (int k: intNumbers) {
-                System.out.println(k);
-            }
+            System.out.println("The author refers to the figures consistently.");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
